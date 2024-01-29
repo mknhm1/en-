e452b75a05e3daf175d1f257762e0c91a3c33eca
mknhm1 <150322694+mknhm1@users.noreply.github.com>
2024-01-29 13:53:47 +0900

    Add files via upload

---
+++ ennsyuu/en14/en_14_5/en_14_5.java
@@ -0,0 +1,14 @@
+package en14.en_14_5;
+
+
+public class en_14_5 {
+    public static void main(String[] args) {
+        StringBuilder str1 = new StringBuilder();
+        str1.append("東京都千代田区");
+        str1.append("神田神保町");
+        System.out.println(str1.toString());
+    }
+}
+
+
+
